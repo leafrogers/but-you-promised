5b322506fac9206d0e0246847394037974b50571
Keith Rogers <git@keithrogers.eu>
2018-08-08 16:11:48 +0100

    Allow backoff strategies
    Usage: add a backoffStrategy function to the settings object you pass in
    when calling the module. See the README for a full example.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const defaults = {
+	backoffStrategy: () => 0,
 	triesRemaining: 5
 };
 
@@ -11,14 +12,22 @@ module.exports = (func, options) => {
 		return Promise.reject(new Error(`The first parameter should be a function, but it was type ${typeof func}`));
 	}
 
+	let attemptNum = 0;
+
 	const attempt = () => {
+		attemptNum++;
 		settings.triesRemaining--;
 
 		return func(settings.data)
 			.catch(err => {
 				if (settings.triesRemaining) {
-					return attempt();
-				}
+					return new Promise(resolve => {
+						setTimeout(
+							() => resolve(attempt()),
+							settings.backoffStrategy(attemptNum)
+						);
+					});
+				};
 
 				return Promise.reject(err);
 			});
